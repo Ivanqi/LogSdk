77a1e1288640fd7406ccf7dc2f66275d9f154dcb
Ivanqi <512350225@qq.com>
2020-02-22 20:48:50 +0800

    【优化】增加新的错误提示

--- src/Exception/TcpClientException.php
+++ src/Exception/TcpClientException.php
@@ -8,6 +8,8 @@ class TcpClientException extends \Exception
     const PORT_IS_EMPTY = '端口为空';
     const DATA_IS_EMPTY = '数据为空';
     const RECV_TIMEOUT = '读取数据超时';
+    const CONNECT_FAILED = '连接失败';
+    const DATA_SEND_FAILED = '数据发送失败';
 
     const ERROR_CODE = 0;
 
